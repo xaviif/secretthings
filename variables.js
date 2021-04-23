6ed56a2b542ba5fb55d51a1470f51d7f08f6c847
xavi <fbjavi023@gmail.com>
2021-04-23 02:03:03 -0400

    Initial commit

---
+++ variables.js
@@ -0,0 +1,8 @@
+var month1 = [
+  {"ACCID":"1","Column2":"dadsa","DATE":"01/01/2001","Col7":"234342","Column5":"432432"},
+  {"ACCID":"2","Column2":"gfsgsf","DATE":"01/01/2001","Col7":"42342","Column5":"3423432"},
+  {"ACCID":"3","Column2":"bfsbfs","DATE":"02/02/2001","Col7":"532532532","Column5":"53253253"},
+  {"ACCID":"1","Column2":"bsffsbfsbs","DATE":"01/03/2001","Col7":"532532","Column5":"53253253"},
+  {"ACCID":"4","Column2":"bfsfbssfb","DATE":"05/02/2001","Col7":"532532","Column5":"532532"},
+  {"ACCID":"3","Column2":"bffbssfbs","DATE":"07/02/2001","Col7":"25378870","Column5":"532253"}
+  ]
